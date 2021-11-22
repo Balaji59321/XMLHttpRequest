268b73140181e386410009fc44bce613c90942be
Mvl-Balaji <balaji.narasimhan@fourkites.com>
2021-11-22 13:32:54 +0530

    resolved static iteration

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ req.onload = function () {
   // console.log(this.response[0]);
   let id_Arr = [];
   let res = JSON.parse(this.response);
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < res.length; i++) {
     console.log(res[i]["id"]);
     id_Arr.push(res[i]["id"]);
   }
